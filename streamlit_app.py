a8472bace98be1543271845a92847c04b18c771c
Ioann Jeanlao Gorvier <aquonelocal@gmail.com>
2024-03-11 03:05:32 +0500

    Modified: streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,11 @@ from alpha_vantage.cryptocurrencies import CryptoCurrencies
 from alpha_vantage.foreignexchange import ForeignExchange
 
 
-quandl.ApiConfig.api_key = os.environ.get("f3ueibFkSNJ-Mt3jpMgq")
-ts = TimeSeries(key=os.environ.get("MRJWA5DCD0F57BFZ"), output_format="pandas")
+quandl.ApiConfig.api_key = os.environ.get("YOUR_QUANDL_API_KEY")
+ts = TimeSeries(
+    ALPHAVANTAGE_API_KEY=os.environ.get("YOUR_ALPHA_VANTAGE_API_KEY"),
+    output_format="pandas",
+)
 
 st.markdown(
     f"Demo app showing **Closing price** and daily **APR change** of a selected ticker from Alpha Vantage API"
